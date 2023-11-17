3a61e87038b2982fb119ed89f16708ded30f6bee
gagan <136232083+gagan283@users.noreply.github.com>
2023-11-17 08:28:30 +0000

    discuss datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let Name = "name"
+let age = 19
+let isLoggedIn = false
+
+
+// primative datatypes 
+
+/*
+
+number => 2 to the power 53
+bigint
+string => ""
+boolean = true or false
+null => standalone Value 
+undefined => not defined yet 
+symbol => unique
+*/
+
+objects
