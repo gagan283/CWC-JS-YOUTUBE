eb9d805d9201ff6010b1d14a091c7dd1759538fc
gagan <136232083+gagan283@users.noreply.github.com>
2023-11-17 08:28:15 +0000

    discuss variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "12345"
+let accountEmail = "name@mail.com"
+var accountPassword = "123456"
+accountCity = "Delhi"
+
+
+accountEmail = "name2@mail.com"
+accountPassword = "654321"
+accountCity = "dilli"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
