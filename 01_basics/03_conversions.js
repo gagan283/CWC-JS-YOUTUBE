91e8f7dbfeb7c7fbded46b581e0adae61f2053bb
gagan <136232083+gagan283@users.noreply.github.com>
2023-11-17 08:28:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,21 @@
+let score = "22vv"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => number is 33
+// "33abc" => NaN
+// true = 1; false = 0
+
+// let isLoggedIn = 0
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+let anyNumber = 33
+let stringNumber = String(anyNumber)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
